8910782ad777c1944eea29700fa0547d40ed5bde
Corey DeRossett <cderossett3@gmail.com>
2024-01-30 08:48:05 -0500

    added comments to arithmetic assignments

--- src/Main.java
+++ src/Main.java
@@ -16,22 +16,22 @@ public class Main {
         double doubleDifference = 35.5;
         double doubleQuotient = 44.5;
         double doubleModulo = 50.5;
-        intSum = intOperandA + intOperandB;
-        intProduct = intOperandA * intOperandB;
-        intDifference = intOperandA - intOperandB;
-        intQuotient = intOperandA / intOperandB;
-        intModulo = intOperandA % intOperandB;
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmetic operator +
+        intProduct = intOperandA * intOperandB; // Assignment that uses the arithmetic operator *
+        intDifference = intOperandA - intOperandB; // Assignment that uses the arithmetic operator -
+        intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmetic operator /
+        intModulo = intOperandA % intOperandB; // Assignment that uses the arithmetic operator %
         System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum); // Output statement
         System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct); // Output statement
         System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference); // Output statement
         System.out.println("The quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient); // Output statement
         System.out.println("The modulo using ints of " + intOperandA + " " + intOperandB + " is " + intModulo); // Output statement
-        doubleSum = doubleOperandA + doubleOperandB;
-        doubleProduct = doubleOperandA * doubleOperandB;
-        doubleDifference = doubleOperandA - doubleOperandB;
-        doubleQuotient = doubleOperandA / doubleOperandB;
-        doubleModulo = doubleOperandA % doubleOperandB;
-        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+        doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmetic operator +
+        doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmetic operator *
+        doubleDifference = doubleOperandA - doubleOperandB; // Assignment that uses the arithmetic operator -
+        doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmetic operator /
+        doubleModulo = doubleOperandA % doubleOperandB; // Assignment that uses the arithmetic operator %
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum); // Output statement
 
 
     }
