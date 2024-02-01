787726d5e78a5c67863e5e10949470349c8c0bf0
Corey DeRossett <cderossett3@gmail.com>
2024-02-01 09:30:34 -0500

    Added new java class "Declarations" and declared variables in it

---
+++ src/Declarations.java
@@ -0,0 +1,12 @@
+public class Declarations {
+    public static void main(String[] args) {
+        double myLunchCost = 12.50;
+        int kidsInFamily = 6;
+        boolean isRaining = false;
+        double gallonGasPrice = 3.99;
+        int favNum = 33;
+        double shoeSize = 10.5;
+        int birthMonth = 11;
+        String fullName = "Corey DeRossett";
+    }
+}
